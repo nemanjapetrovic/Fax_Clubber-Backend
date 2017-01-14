554538b0c778b42908943a739a461be2a221371b
Nemanja Petrovic <nemanjapetrovic1794@gmail.com>
2017-01-14 15:23:20 +0100

    Neo4j repo manager is finished

--- Project/ClubberServices/Clubber.Backend.Neo4jDB/Neo4jManagers/Neo4jManager.cs
+++ Project/ClubberServices/Clubber.Backend.Neo4jDB/Neo4jManagers/Neo4jManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Clubber.Backend.Neo4jDB.Neo4jRepository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,29 @@ namespace Clubber.Backend.Neo4jDB.Neo4jManagers
 {
     public class Neo4jManager
     {
+        private string _connectionString;
 
+        protected INeo4jRepository _neoRepo = null;
+        public INeo4jRepository Neo4jRepository
+        {
+            get
+            {
+                if (_neoRepo == null)
+                {
+                    _neoRepo = new Neo4jRepository.Neo4jRepository(_connectionString);
+                }
+                return _neoRepo;
+            }
+        }
+
+        public Neo4jManager(string connectionString)
+        {
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new Exception("Neo4j connection string is empty!");
+            }
+
+            _connectionString = connectionString;
+        }
     }
 }
