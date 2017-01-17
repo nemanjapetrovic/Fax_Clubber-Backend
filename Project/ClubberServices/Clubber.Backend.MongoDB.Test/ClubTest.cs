5d35314334ef3133cbdae2def0b825c592f646f3
Nemanja Petrovic <nemanjapetrovic1794@gmail.com>
2017-01-18 00:20:27 +0100

    Added unit test for MongoDB

---
+++ Project/ClubberServices/Clubber.Backend.MongoDB.Test/ClubTest.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MongoDB.Bson;
+using Clubber.Backend.Models.Model;
+using Clubber.Backend.MongoDB.MongoManagers;
+using System.Linq;
+
+namespace Clubber.Backend.MongoDB.Test
+{
+    [TestClass]
+    public class ClubTest
+    {
+        private ClubMongoManager _club = new ClubMongoManager("mongodb://localhost:27017", "clubbertest");
+
+        [TestMethod]
+        public void GetAll()
+        {
+            var items = _club.ClubRepository.Get();
+
+            Assert.IsNotNull(items);
+        }
+
+        [TestMethod]
+        public void Get()
+        {
+            // Get all
+            var items = _club.ClubRepository.Get();
+            Assert.IsNotNull(items);
+
+            // Convert to list
+            var listItems = items.ToList<Club>();
+            Assert.IsNotNull(listItems);
+
+            if (listItems.Count == 0)
+            {
+                return;
+            }
+
+            // Get only one
+            ObjectId id = new ObjectId(listItems[0]._id.ToString());
+            var item = _club.ClubRepository.Get(id);
+
+            Assert.IsNotNull(item);
+        }
+
+        [TestMethod]
+        public void Add()
+        {
+            Club club = new Club()
+            {
+                Name = "Nis Nocu",
+                Country = "Srbija",
+                Address = "KOMREN",
+                PhoneNumber = { "018800619", "21812912" },
+                Email = { "swaba@test.com", "test@test.com" },
+                Image = "img test",
+                Website = "url url web",
+                Facebook = "url url facebook",
+                Instagram = "url url instagram",
+                Twitter = "url url twitter"
+            };
+            _club.ClubRepository.Add(club);
+
+            Assert.IsNotNull(club._id);
+        }
+
+        [TestMethod]
+        public void Update()
+        {
+            // Get all
+            var items = _club.ClubRepository.Get();
+            Assert.IsNotNull(items);
+
+            // Convert to list
+            var listItems = items.ToList<Club>();
+            Assert.IsNotNull(listItems);
+
+            if (listItems.Count == 0)
+            {
+                return;
+            }
+
+            // Get only one
+            ObjectId id = new ObjectId(listItems[0]._id.ToString());
+            var item = _club.ClubRepository.Get(id);
+
+            //Update
+            var r = new Random();
+            var ran = r.Next(100);
+            Club club = new Club()
+            {
+                Name = "Nis Nocu",
+                Country = "Srbija",
+                Address = "DonjaVrezina Test",
+                PhoneNumber = { "018800619", "21812912" },
+                Email = { "swaba@test.com", "test@test.com" },
+                Image = "img test",
+                Website = "url url web " + ran.ToString(),
+                Facebook = "url url facebook " + ran.ToString(),
+                Instagram = "url url instagram " + ran.ToString(),
+                Twitter = "url url twitter " + ran.ToString()
+            };
+            club._id = id;
+            bool added = _club.ClubRepository.Update(x => x._id, club._id, club);
+            bool test = true;
+
+            Assert.IsNotNull(club._id);
+            Assert.AreEqual(added, test);
+        }
+
+        [TestMethod]
+        public void Delete()
+        {
+            // Get all
+            var items = _club.ClubRepository.Get();
+            Assert.IsNotNull(items);
+
+            // Convert to list
+            var listItems = items.ToList<Club>();
+            Assert.IsNotNull(listItems);
+
+            if (listItems.Count == 0)
+            {
+                return;
+            }
+
+            // Get only one
+            ObjectId id = new ObjectId(listItems[0]._id.ToString());
+            var deletedClub = _club.ClubRepository.Delete(item => item._id, id);
+
+            Assert.IsNotNull(deletedClub);
+        }
+    }
+}
---
+++ Project/ClubberServices/Clubber.Backend.MongoDB.Test/UserTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MongoDB.Bson;
+using System.Linq.Expressions;
+using Clubber.Backend.Models.Model;
+
+namespace Clubber.Backend.MongoDB.Test
+{
+    [TestClass]
+    public class UserTest
+    {
+        [TestMethod]
+        public void GetAll()
+        {
+
+        }
+
+        [TestMethod]
+        public void Get()
+        {
+
+        }
+
+        [TestMethod]
+        public void Add()
+        {
+
+        }
+
+        [TestMethod]
+        public void Update()
+        {
+
+        }
+
+        [TestMethod]
+        public void Delete()
+        {
+
+        }
+    }
+}
