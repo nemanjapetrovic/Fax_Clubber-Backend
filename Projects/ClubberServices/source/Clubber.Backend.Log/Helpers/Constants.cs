6b06be13a1413ecf5c2692afb871ec9affe6db71
Nemanja Petrovic <nemanjapetrovic1794@gmail.com>
2017-02-16 19:02:02 +0100

    Added log project , gitignore made problem with ignoring *.log files

---
+++ Projects/ClubberServices/source/Clubber.Backend.Log/Helpers/Constants.cs
@@ -0,0 +1,11 @@
+﻿namespace Clubber.Backend.WebAPI.Helpers
+{
+    internal static class Constants
+    {
+        internal class MongoDB
+        {
+            internal static string MongoDBConectionString = "MongoDBConectionString";
+            internal static string MongoDBDatabaseName = "MongoDBDatabaseName";
+        }
+    }
+}
---
+++ Projects/ClubberServices/source/Clubber.Backend.Log/LogServices/LogService.cs
@@ -0,0 +1,50 @@
+﻿using Clubber.Backend.MongoDB.MongoManagers;
+using Clubber.Backend.WebAPI.Helpers;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using LogModel = Clubber.Backend.Models.LogModels.Log;
+
+namespace Clubber.Backend.Log.LogServices
+{
+    public class LogService
+    {
+        private readonly LogMongoManager _mongoLogManager;
+
+        public LogService()
+        {
+            string mongoConStr = ConfigurationManager.AppSettings[Constants.MongoDB.MongoDBDatabaseName];
+            string mongoDbName = ConfigurationManager.AppSettings[Constants.MongoDB.MongoDBDatabaseName];
+
+            _mongoLogManager = new LogMongoManager(mongoConStr, mongoDbName);
+        }
+
+        public void Add(LogModel entity)
+        {
+            _mongoLogManager.LogRepository.Add(entity);
+        }
+
+        public IQueryable<LogModel> Get()
+        {
+            return _mongoLogManager.LogRepository.Get();
+        }
+
+        public IQueryable<LogModel> Get(string id)
+        {
+            var log = _mongoLogManager.LogRepository.Get(id);
+            IList<LogModel> list = new List<LogModel>();
+
+            if (log != null)
+            {
+                list.Add(log);
+            }
+
+            return list.AsQueryable();
+        }
+
+        public void Delete(string id)
+        {
+            _mongoLogManager.LogRepository.Delete(item => item._id, id);
+        }
+    }
+}
